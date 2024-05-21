61bddaf99830966a9e25bd22a8d99016bffad447
kaiyumdev <afrankaiyum34@gmail.com>
2024-05-21 13:08:28 +0600

    feat(googleLogin): Implements googleLogin onClick button successfully

--- src/components/Login-Registration/GoogleLogin.jsx
+++ src/components/Login-Registration/GoogleLogin.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import { FaGoogle } from "react-icons/fa";
+import useAuth from '../../hooks/useAuth';
 
 const GoogleLogin = () => {
+  const {googleLogin} = useAuth;
+  const handleGoogleSingIn = () => {
+    googleLogin()
+  }
   return (
-    <button className='btn w-full'>
+    <button onClick={handleGoogleSingIn} className='btn w-full'>
        <div className='flex items-center gap-2'>
        <FaGoogle />
        <p>Google</p>
