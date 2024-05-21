0d29bb2b4b6e4b968e76c0e6f491d1a26aca6e6d
kaiyumdev <afrankaiyum34@gmail.com>
2024-05-22 00:05:25 +0600

    feat(location): solve issue of reloading dashboard to automatic loading dashboard page

--- src/pages/Registrations.jsx
+++ src/pages/Registrations.jsx
@@ -31,6 +31,7 @@ const Registrations = () => {
       navigate(from, {replace: true})
     }
   },[user, navigate, from])
+  
   return (
     <form onSubmit={handleSubmit} className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col lg:flex-row-reverse">
