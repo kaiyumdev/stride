f841dde656b7163a878ed64b7b1eb2a0fba0d59a
kaiyumdev <afrankaiyum34@gmail.com>
2024-05-26 04:12:53 +0600

    feat(getAllData): get all products data from json server

--- src/pages/AllProducts.jsx
+++ src/pages/AllProducts.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import SingleProduct from '../components/home/SingleProduct';
 
 const AllProducts = () => {
+    const [products, setProducts] = useState();
+    useEffect(() => {
+        fetch("http://localhost:3000/shoes")
+        .then((res) => res.json())
+        .then((data) => setProducts(data))
+    },[])
   return (
-    <div>AllProducts</div>
+    <div className='flex justify-center gap-16 flex-wrap'>
+        {
+            products?.map((product) => <SingleProduct key={product.id} product={product}></SingleProduct>)
+        }
+    </div>
   )
 }
 
