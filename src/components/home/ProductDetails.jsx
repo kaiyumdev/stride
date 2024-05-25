2e60552916626b951bca19ffc68f5a5e8df23569
kaiyumdev <afrankaiyum34@gmail.com>
2024-05-26 03:27:18 +0600

    feat(productDetails): create productDetails page to show singleProduct details

--- src/components/home/ProductDetails.jsx
+++ src/components/home/ProductDetails.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 import { useLoaderData } from 'react-router-dom'
 
 const ProductDetails = () => {
-    const shoe = useLoaderData()
+    const shoe = useLoaderData();
+    const {title, brand, price, description, image_url} = shoe;
   return (
-    <div>ProductDetails: {shoe.id}</div>
+    <div className=' flex justify-center'>
+        <div className="card w-96 glass">
+  <figure><img src={image_url} alt="shoe!"/></figure>
+  <div className="card-body">
+    <h2 className="card-title">{title}</h2>
+    <h3 className="text-xl font-semibold">{brand}</h3>
+    <h3 className="text-xl font-semibold">{price}</h3>
+    <p>{description}</p>
+  </div>
+</div>
+    </div>
   )
 }
 
