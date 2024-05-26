71dd2e2d69d7b5aab95775f63183f8b110486708
kaiyumdev <afrankaiyum34@gmail.com>
2024-05-26 12:47:07 +0600

    feat(edit): Implements editProduct functionality successfully

--- src/pages/EditProduct.jsx
+++ src/pages/EditProduct.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
 
 const EditProduct = () => {
+    const shoe = useLoaderData()
+
+    const [id, setId] = useState(shoe.id)
+    const [title, setTitle] = useState(shoe.title)
+    const [price, setPrice] = useState(shoe.price)
+    const [brand, setBrand] = useState(shoe.brand)
+    const [description, setDescription] = useState(shoe.description)
+    const [image_url, setImageUrl] = useState(shoe.image_url)
     const handleSubmit = async(e) => {
         e.preventDefault()
         const form = e.target;
@@ -11,6 +20,11 @@ const EditProduct = () => {
         const description =  form.description.value;
         const image_url = form.image_url.value;
         const data = {id, title, brand, price, description, image_url}
+        await fetch(`http://localhost:3000/shoes/${shoe.id}`, {
+            method: "PATCH",
+            headers: {"Content-type": "application/json"},
+            body: JSON.stringify(data)
+        }).then((res) => res.json()).then((data) => console.log(data))
     }
   return (
     <div>
@@ -18,25 +32,25 @@ const EditProduct = () => {
         <div className='my-16'>
             <form onSubmit={handleSubmit}>
                 <div className='mt-2'>
-                    <input className='bg-gray-100 p-4 w-full border border-black rounded-lg ' type="text" name="title" id="" placeholder='title'/>
+                    <input className='bg-gray-100 p-4 w-full border border-black rounded-lg ' value={title} onChange={(e) => setTitle(e.target.value)} type="text" name="title" id="" placeholder='title'/>
                 </div>
                 <div className='mt-2'>
-                    <input className='bg-gray-100 p-4 w-full border border-black rounded-lg ' type="text" name="brand" id="" placeholder='brand'/>
+                    <input className='bg-gray-100 p-4 w-full border border-black rounded-lg ' value={brand} onChange={(e) => setBrand(e.target.value)} type="text" name="brand" id="" placeholder='brand'/>
                 </div>
                 <div className='mt-2'>
-                    <input className='bg-gray-100 p-4 w-full border border-black rounded-lg ' type="text" name="price" id="" placeholder='price' />
+                    <input className='bg-gray-100 p-4 w-full border border-black rounded-lg ' value={price} onChange={(e) => setPrice(e.target.value)} type="text" name="price" id="" placeholder='price' />
                 </div>
                 <div className='mt-2'>
-                    <input className='bg-gray-100 p-4 w-full border border-black rounded-lg ' type="text" name="description" id="" placeholder='description'/>
+                    <input className='bg-gray-100 p-4 w-full border border-black rounded-lg ' value={description} onChange={(e) => setDescription(e.target.value)} type="text" name="description" id="" placeholder='description'/>
                 </div>
                 <div className='mt-2'>
-                    <input className='bg-gray-100 p-4 w-full border border-black rounded-lg ' type="text" name="image_url" id="" placeholder='image_url'/>
+                    <input className='bg-gray-100 p-4 w-full border border-black rounded-lg ' value={image_url} onChange={(e) => setImageUrl(e.target.value)} type="text" name="image_url" id="" placeholder='image_url'/>
                 </div>
                 <div className='mt-2'>
-                    <input className='bg-gray-100 p-4 w-full border border-black rounded-lg ' type="text" name="id" id="" placeholder='id'/>
+                    <input className='bg-gray-100 p-4 w-full border border-black rounded-lg ' value={id} onChange={(e) => setId(e.target.value)} type="text" name="id" id="" placeholder='id'/>
                 </div>
                 <div className='mt-2 flex justify-center items-center'>
-                    <input className='btn mt-4 w-full'  type="submit" value="Add product" />
+                    <input className='btn mt-4 w-full'  type="submit" value="Edit product" />
                 </div>
             </form>
         </div>
